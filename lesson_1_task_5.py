dab92436556ac8d59aadd1150f7381fe8569918b
PodkovyrovaN <natakam0310@gmail.com>
2025-07-14 20:06:58 +0500

    добавление дз1

---
+++ lesson_1_task_5.py
@@ -0,0 +1,13 @@
+def eight(): print(8, end="")
+def eight(): print(8, end="")
+def zero(): print(0, end="")
+def zero(): print(0, end="")
+def five(): print(5, end="")
+def five(): print(5, end="")
+def five(): print(5, end="")
+def three(): print(3, end="")
+def five(): print(5, end="")
+def three(): print(3, end="")
+def five(): print(5, end="")
+
+eight(), eight(), zero (), zero (), five(), five(), five(), three(), five(), three(), five()
