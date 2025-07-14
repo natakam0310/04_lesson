9d3af611c6f9c143c7eb6bb05c14bb80067cd77d
PodkovyrovaN <natakam0310@gmail.com>
2025-07-14 20:11:58 +0500

    добавление дз 2

---
+++ card.py
@@ -0,0 +1,15 @@
+class Card:
+    number = '0000 0000 0000 0000'
+    validDate = '01/99'
+    holder = 'unknown'
+
+
+    def __init__(self, number, date, holder):
+        self.number = number
+        self.holder = holder
+        self.validDate = date
+
+    def ply (self, amount):
+        print("с карты", self.number, "списали", amount)    
+        
+		
