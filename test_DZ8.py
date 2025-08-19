9cdc658e9851bf423c3c73e0ebf18c355b84be22
PodkovyrovaN <natakam0310@gmail.com>
2025-08-19 21:57:50 +0500

    исправление домашки8

--- test_DZ8.py
+++ test_DZ8.py
@@ -15,13 +15,13 @@ HEADERS = {
 
 @pytest.mark.positive
 def test_create_project_success():
-    my_proekt = {
-    "title": "ГосУслуги",
+    my_project = {
+    "title": "Госуслуги",
     "users": {
     "66a89519-0718-4600-93b1-72c61f2074e7": "worker"}
 }
     response = requests.post(BASE_URL + '/projects',
-    json=my_proekt, headers=HEADERS)
+    json=my_project, headers=HEADERS)
     assert response.status_code == 201
 
 
@@ -36,30 +36,30 @@ def test_create_project_missing_name():
 
 # --- PUT /api-v2/projects/{id} ---
 @pytest.mark.positive
-def test_PUT_project_id():
+def test_put_project_id():
     project_id = "e3dee103-5604-4c87-8e01-faa800997065"
-    my_proekt_id = {
+    my_project_id = {
     "deleted": True,
-    "title": "ГосУслуги",
+    "title": "Госуслуги",
     "users": {
     "66a89519-0718-4600-93b1-72c61f2074e7": "worker"}
 }
     response = requests.put(f"{BASE_URL}/projects/{project_id}",
-    json=my_proekt_id, headers=HEADERS)
+    json=my_project_id, headers=HEADERS)
     assert response.status_code == 200
 
 
 @pytest.mark.negative
-def test_PUT_negative_id():
+def test_put_negative_id():
     project_id = " "
-    my_proekt_id = {
+    my_project_id = {
     "deleted": True,
     "title": "ГосУслуги",
     "users": {
     "66a89519-0718-4600-93b1-72c61f2074e7": "worker"}
 }
     response = requests.put(f"{BASE_URL}/projects/{project_id}",
-    json=my_proekt_id, headers=HEADERS)
+    json=my_project_id, headers=HEADERS)
     assert response.status_code == 404
 
 
@@ -71,7 +71,7 @@ def test_get_project():
 
 
 @pytest.mark.negative
-def test_create_negative_GET():
+def test_create_negative_get():
  project_id = ""
  url = f"{BASE_URL}/projects/{project_id}"
  response = requests.put(url, headers=HEADERS)
